a29de59a0602419082b09b6ecb5cc14083b53225
Mohit Chauhan <mohitchauhan@Mac.lan>
2025-12-30 13:33:43 +0530

    Initial commit: JS AI full-stack platform

---
+++ index.ts
@@ -0,0 +1,16 @@
+import concurrency from 'concurrently';
+
+concurrency([
+   {
+      name: 'server',
+      command: 'bun run dev',
+      cwd: 'packages/server',
+      prefixColor: 'cyan',
+   },
+   {
+      name: 'client',
+      command: 'bun run dev',
+      cwd: 'packages/client',
+      prefixColor: 'green',
+   },
+]);
